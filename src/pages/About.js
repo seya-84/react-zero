23d5c41212586e7ddeebfd34f79aeef1098af824
Yannick Severo <yannicksevero@gmail.com>
2021-03-28 09:27:40 +0200

    add navigation

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const About = () => {
   return (
     <div>
+      <Navigation />
       <h1>À propos</h1>
       <br />
       <p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return(
     <div className="home">
+      <Navigation />
       <h1>Accueil</h1>
     </div>
   )
