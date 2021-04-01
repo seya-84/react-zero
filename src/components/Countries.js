d16c581113e23f2993f74aa390cb3530bbf3c4fe
Yannick Severo <yannicksevero@gmail.com>
2021-04-01 21:07:50 +0200

    add input type=range

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,20 +4,42 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [sortedData, setSortedData] = useState ([]);
+  const [playOnce, setPlayOnce] = useState(true);
+  const [rangeValue, setRangeValue] = useState(40);
 
   useEffect(() => { 
-    axios
-      .get(
-        "http://restcountries.eu/rest/v2/all?fiels=name;population;region;flag"
-      )
-      .then((res) => setData(res.data));
+    if (playOnce) {
+      axios
+        .get(
+          "http://restcountries.eu/rest/v2/all?fiels=name;population;region;flag"
+        )
+        .then((res) => {
+          setData(res.data);
+          setPlayOnce(false);
+        });
+    }
 
-  }, []); 
+    const sortedCountry = () => {
+      const countryObj = Object.keys(data).map((i) => data[i]);
+      const sortedArray = countryObj.sort((a, b) => {
+        return b.population - a.population
+      });
+      sortedArray.length = rangeValue;
+      setSortedData(sortedArray);
+    };  
+    sortedCountry();
+
+  }, [data, rangeValue]); 
 
   return (
     <div className="countries">
+      <div className="sort-container">
+        <input type="range" min="1" max="250" value={rangeValue} 
+        onChange={(e) => setRangeValue(e.target.value)}/>
+      </div>
       <ul className="countries-list">
-        {data.map((country) => (
+        {sortedData.map((country) => (
           <Card country={country} key={country.name}/>
         ))}
       </ul>
